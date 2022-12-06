c832cdef442389779a726e7dbefea872e71cc076
nordgaren <nordgarentv@gmail.com>
2022-12-06 02:58:48 -0700

    Spelling mistake

--- NIF2FBX/CommandLineOptions.cs
+++ NIF2FBX/CommandLineOptions.cs
@@ -6,25 +6,19 @@ namespace NIF2FBX2FLVER {
             
         [Value(0, MetaName = "Blender Path", HelpText = "Path to Blender exe.", Required = true)]
         public string BlenderPath { get; set; }
-        
         [Value(1, MetaName = "Morrowind Path", HelpText = "Path to unpacked Morrowind folder.", Required = true)]
         public string MorrowindPath { get; set; }
-
         [Option('p', "python", Required = false, HelpText = "NIF conversion python script path. Defaults to inside this exe's folder + mass_convert_nif.py")]
         public string ScriptPath { get; set; }
-    
         [Option('i', "input", Required = false, HelpText = "Folder to convert Morrowwind Path + \\Data Files\\meshes\\")]
         public string InputFolder { get; set; }
-    
         [Option('x', "tex", Required = false, HelpText = "Morrowwind Texture Folder. Defaults to Morrowwind Path + \\Data Files\\textures\\")]
         public string TextureFolder { get; set; }
-    
         [Option('r', "recursive", Required = false, HelpText = "Switch for searching subdirectories for nif files.")]
         public bool Recursive { get; set; }
-    
         [Option('s', "skip", Required = false, HelpText = "Switch to skip files that have already been converted. On by default.")]
         public bool Skip { get; set; }
-        [Option('w', "wait", Required = false, HelpText = "Switch to wait for the current blender process before going to the next one. Ony by default")]
+        [Option('w', "wait", Required = false, HelpText = "Switch to wait for the current blender process before going to the next one. On by default")]
         public bool Wait { get; set; }
     }
 
